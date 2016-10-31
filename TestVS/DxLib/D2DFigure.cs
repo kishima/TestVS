7edc154d6da51a6d4c1812fba56d9146c4670616
kishima <kishima7@gmail.com>
2016-11-01 07:28:12 +0900

    add bitmap demo

---
+++ TestVS/DxLib/D2DFigure.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using SharpDX;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Direct2D1;
+using SharpDX.DXGI;
+
+using AlphaMode = SharpDX.Direct2D1.AlphaMode;
+using Bitmap = SharpDX.Direct2D1.Bitmap;
+using PixelFormat = SharpDX.Direct2D1.PixelFormat;
+
+
+namespace DxLib
+{
+    public class D2DFigure
+    {
+
+        SharpDX.Direct3D11.Device _device;
+        Texture2D _backBuffer;
+        RenderTargetView _backBufferView;
+
+        public SharpDX.Direct2D1.Factory Factory2D { get; private set; }
+        public RenderTarget RenderTarget2D { get; private set; }
+        private Bitmap _bitmap;
+
+        public SharpDX.Direct3D11.Device Device
+        {
+            get
+            {
+                return _device;
+            }
+        }
+
+        public Texture2D BackBuffer
+        {
+            get
+            {
+                return _backBuffer;
+            }
+        }
+
+        public RenderTargetView BackBufferView
+        {
+            get
+            {
+                return _backBufferView;
+            }
+        }
+
+        public D2DFigure()
+        {
+            Factory2D = new SharpDX.Direct2D1.Factory();
+
+            using (var surface = BackBuffer.QueryInterface<Surface>())
+            {
+                RenderTarget2D = new RenderTarget(Factory2D, surface,
+                                                  new RenderTargetProperties(new PixelFormat(Format.Unknown, SharpDX.Direct2D1.AlphaMode.Premultiplied)));
+            }
+            RenderTarget2D.AntialiasMode = AntialiasMode.PerPrimitive;
+
+        }
+
+        public static Bitmap LoadFromFile(RenderTarget renderTarget, string file)
+        {
+            // Loads from file using System.Drawing.Image
+            using (var bitmap = (System.Drawing.Bitmap)System.Drawing.Image.FromFile(file))
+            {
+                var sourceArea = new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                var bitmapProperties = new BitmapProperties(new PixelFormat(Format.R8G8B8A8_UNorm, AlphaMode.Premultiplied));
+                var size = new Size2(bitmap.Width, bitmap.Height);
+
+                // Transform pixels from BGRA to RGBA
+                int stride = bitmap.Width * sizeof(int);
+                using (var tempStream = new DataStream(bitmap.Height * stride, true, true))
+                {
+                    // Lock System.Drawing.Bitmap
+                    var bitmapData = bitmap.LockBits(sourceArea, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+
+                    // Convert all pixels 
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        int offset = bitmapData.Stride * y;
+                        for (int x = 0; x < bitmap.Width; x++)
+                        {
+                            // Not optimized 
+                            byte B = Marshal.ReadByte(bitmapData.Scan0, offset++);
+                            byte G = Marshal.ReadByte(bitmapData.Scan0, offset++);
+                            byte R = Marshal.ReadByte(bitmapData.Scan0, offset++);
+                            byte A = Marshal.ReadByte(bitmapData.Scan0, offset++);
+                            int rgba = R | (G << 8) | (B << 16) | (A << 24);
+                            tempStream.Write(rgba);
+                        }
+
+                    }
+                    bitmap.UnlockBits(bitmapData);
+                    tempStream.Position = 0;
+
+                    return new Bitmap(renderTarget, size, tempStream, stride, bitmapProperties);
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            RenderTarget2D.DrawBitmap(_bitmap, 1.0f, BitmapInterpolationMode.Linear);
+
+        }
+
+        void BeginDraw()
+        {
+            RenderTarget2D.BeginDraw();
+        }
+
+        void EndDraw()
+        {
+            RenderTarget2D.EndDraw();
+
+        }
+    }
+}
--- TestVS/TestVS/Form1.cs
+++ TestVS/TestVS/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DxLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,15 +15,19 @@ namespace TestVS
     public partial class Form1 : Form
     {
         private Log LogOutput;
+        D2DFigure figure;
+
         public Form1()
         {
             InitializeComponent();
             LogOutput = new Log(textBox1);
+            figure = new D2DFigure();
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        {            
             LogOutput.Put("button1 is pressed");
+            figure.Draw();
         }
     }
 }
